a1bd1c95368e87251a74c96552ecb1cc4c525d1d
Sergio <feruanos@unican.es>
2019-09-11 16:26:16 +0200

    Added files in order to keep track of the version of the application as well as to define the version in the executable properties

---
+++ src/version.hpp
@@ -0,0 +1,31 @@
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+ 
+#define VERSION_MAJOR               0
+#define VERSION_MINOR               0
+#define VERSION_REVISION            0
+#define VERSION_BUILD               0
+ 
+#define VER_FILE_DESCRIPTION_STR    "Offshore dynamics simulator"
+#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
+#define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
+                                    "." STRINGIZE(VERSION_MINOR)    \
+                                    "." STRINGIZE(VERSION_REVISION) \
+                                    "." STRINGIZE(VERSION_BUILD)    \
+ 
+#define VER_PRODUCTNAME_STR         "OASIS"
+#define VER_PRODUCT_VERSION         VER_FILE_VERSION
+#define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
+#define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".exe"
+#define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
+#define VER_COPYRIGHT_STR           "IHCantabria Copyright (C) 2019"
+ 
+#ifdef _DEBUG
+  #define VER_VER_DEBUG             VS_FF_DEBUG
+#else
+  #define VER_VER_DEBUG             0
+#endif
+ 
+#define VER_FILEOS                  VOS_NT_WINDOWS64
+#define VER_FILEFLAGS               VER_VER_DEBUG
+#define VER_FILETYPE                VFT_APP
