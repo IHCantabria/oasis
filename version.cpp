dff74cb9a3d07e6fd45da52a354a978017d0782f
Alvaro Rodriguez-Luis <rodriguezlua@unican.es>
2026-04-20 18:53:09 +0200

    Updated version.cpp

--- version.cpp
+++ version.cpp
@@ -1,5 +1,5 @@
 // the path to the version.h file is relative to your build directory.
 #include "../src/version.hpp"
 
-const std::string gVERSION = "v0.0.3.2-62-g04a5f9c-d";
-const std::string gVERSION_SHORT = "0.0.3";
+const std::string gVERSION = "v0.1.0";
+const std::string gVERSION_SHORT = "0.1.0";
